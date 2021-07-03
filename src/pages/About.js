c12bdb67efbb3aeb7bd1678480d39ec1f54696d6
treatme030 <treatme8192@gmail.com>
2021-07-03 14:08:00 +0900

    About page 완성

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,152 @@
 import React from 'react';
+import PText from '../components/PText';
+import Button from '../components/Button';
+import AboutImg from '../assets/images/about-page-img.png';
+import styled from 'styled-components';
+import AboutInfoItem from '../components/AboutInfoItem';
+import ContactBanner from '../components/ContactBanner';
+import Footer from '../components/Footer';
+
+const AboutPageStyles = styled.div`
+    padding: 20rem 0 0;
+    .top-section {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 2rem;
+    }
+    .left {
+        flex: 3;
+    }
+    .right {
+        flex: 2;
+    }
+    .about__subheading {
+        font-size: 2.2rem;
+        margin-bottom: 2rem;
+        span {
+            background-color: var(--deep-dark);
+            padding: 0.5rem;
+            border-radius: 8px;
+        }
+    }
+    .about__heading {
+        font-size: 3.6rem;
+        margin-bottom: 3rem;
+    }
+    .about__info {
+        margin-bottom: 4rem;
+        .para {
+            max-width: 100%;
+        }
+    }
+    .right {
+        img {
+            border: 2px solid var(--gray-1);
+        }
+    }
+    .about__info__items {
+        margin-top: 15rem;
+    }
+    .about__info__item {
+        margin-bottom: 10rem;
+    }
+    .about__info__heading {
+        font-size: 3.6rem;
+        text-transform: uppercase;
+    }
+    @media only screen and (max-width: 768px){
+        padding: 10rem 0 0;
+        .top-section {
+            flex-direction: column;
+            gap: 5rem;
+        }
+        .about__subheading {
+            font-size: 1.8rem;
+        }
+        .about__heading {
+            font-size: 2.8rem;
+        }
+        .about__info__heading {
+            font-size: 3rem;
+        }
+    }
+`;
 
 const About = () => {
     return (
-        <div>
-           <h1>This is About Page</h1> 
-        </div>
+        <AboutPageStyles>
+           <div className="container">
+               <div className="top-section">
+                   <div className="left">
+                       <p className="about__subheading">Hi, I am <span>Juhee Kim</span></p>
+                       <h2 className="about__heading">A freelance Web developer</h2>
+                       <div className="about__info">
+                            <PText>
+                                I started coding for one years.
+                                I love it and now I have the opportunity to design 
+                                along with the coding. I find it really interesting 
+                                and I enjoyed the process a lot.
+                                <br/><br/>
+                                My vision is to make the world a better lpace.
+                                Now almost everything is becoming better than ever.
+                                It is time for us to create more good stuff 
+                                that helps the world to become a better place.
+                            </PText>
+                       </div>
+                       <Button btnText="Download CV" btnLink="#"/>
+                   </div>
+                   <div className="right">
+                       <img src={AboutImg} alt="Ayan Khan img"/>
+                   </div>
+               </div>
+               <div className="about__info__items">
+                   <div className="about__info__item">
+                       <h1 className="about__info__heading">EDUCATION</h1>
+                       <AboutInfoItem
+                       title="School"
+                       items={['Nasirbad Govt. High Schllo', 'Chittagong']}
+                       />
+                       <AboutInfoItem
+                       title="College"
+                       items={['BAF shaheen college Chittagong']}
+                       />
+                       <AboutInfoItem
+                       title="Varsity"
+                       items={['University of Chittagong']}
+                       />
+                   </div>
+                   <div className="about__info__item">
+                       <h1 className="about__info__heading">My Skills</h1>
+                       <AboutInfoItem
+                       title="FrontEnd"
+                       items={['HTML', 'CSS', 'JavaScript', 'REACT']}
+                       />
+                       <AboutInfoItem
+                       title="BackEnd"
+                       items={['Node', 'Express', 'PHP']}
+                       />
+                       <AboutInfoItem
+                       title="Design"
+                       items={['Photoshop', 'After Effects', 'Figma']}
+                       />
+                   </div>
+                   <div className="about__info__item">
+                       <h1 className="about__info__heading">Experiences</h1>
+                       <AboutInfoItem
+                       title="2020 - 2021"
+                       items={['vom academy']}
+                       />
+                       <AboutInfoItem
+                       title="2021 -"
+                       items={['codestates']}
+                       />
+                   </div>
+               </div>
+           </div>
+           <ContactBanner/>
+           <Footer/>
+        </AboutPageStyles>
     );
 };
 
