0aa402504da744c62a4d2e6f767ffadfcbe113c7
treatme030 <treatme8192@gmail.com>
2021-07-04 20:35:44 +0900

    removint warnings

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import projectImg from '../assets/images/projectImg.png';
 
 const ProjectItemStyles = styled.div`
     .projectItem__img {
